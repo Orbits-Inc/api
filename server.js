f1bb4655b0a7adcfdeca0ff5552b33448da900ee
Sahil Pabale <dev.sahilpabale@gmail.com>
2021-06-13 17:12:46 +0000

    Configured default server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
